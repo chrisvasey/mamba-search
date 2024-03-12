af88698d178301eb27a30fe05e35956a9322d8e7
Chris Vasey <chrismvasey@gmail.com>
2024-03-12 11:56:31 +0000

    Created landing page

--- resources/js/Pages/LandingPage.tsx
+++ resources/js/Pages/LandingPage.tsx
@@ -4,57 +4,15 @@ import useRoute from '@/Hooks/useRoute';
 import useTypedPage from '@/Hooks/useTypedPage';
 import { Head } from '@inertiajs/react';
 
-interface Props {
-  canLogin: boolean;
-  canRegister: boolean;
-  laravelVersion: string;
-  phpVersion: string;
-}
-
-export default function Welcome({
-  canLogin,
-  canRegister,
-  laravelVersion,
-  phpVersion,
-}: Props) {
+export default function LandingPage() {
   const route = useRoute();
   const page = useTypedPage();
 
   return (
     <>
-      <Head title="Welcome" />
+      <Head title="Welcome to Mamba Search" />
 
       <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        {canLogin ? (
-          <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-            {page.props.auth.user ? (
-              <Link
-                href={route('dashboard')}
-                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-              >
-                Dashboard
-              </Link>
-            ) : (
-              <>
-                <Link
-                  href={route('login')}
-                  className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >
-                  Log in
-                </Link>
-
-                {canRegister ? (
-                  <Link
-                    href={route('register')}
-                    className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                  >
-                    Register
-                  </Link>
-                ) : null}
-              </>
-            )}
-          </div>
-        ) : null}
 
         <div className="max-w-7xl mx-auto p-6 lg:p-8">
           <div className="flex justify-center">
@@ -142,7 +100,7 @@ export default function Welcome({
                   </div>
 
                   <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
-                    Laracasts
+                    Laracastssss
                   </h2>
 
                   <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
@@ -344,7 +302,7 @@ export default function Welcome({
             </div>
 
             <div className="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-              Laravel v{laravelVersion} (PHP v{phpVersion})
+              {/* Laravel v{laravelVersion} (PHP v{phpVersion}) */}
             </div>
           </div>
         </div>
