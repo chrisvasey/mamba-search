334f1d3e30965c58ae1afcc524ec86c4630a2974
Chris Vasey <chrismvasey@gmail.com>
2024-03-12 11:57:11 +0000

    Updated VITE build script to use app URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: 'resources/js/app.tsx',
-      ssr: 'resources/js/ssr.tsx',
-      refresh: true,
-    }),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      '@': '/resources/js',
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [
+      laravel({
+        input: 'resources/js/app.tsx',
+        ssr: 'resources/js/ssr.tsx',
+        refresh: true,
+        detectTls: process.env.VITE_APP_URL,
+      }),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': '/resources/js',
+      },
     },
-  },
-  ssr: {
-    noExternal: ['@inertiajs/server'],
-  },
-});
+    ssr: {
+      noExternal: ['@inertiajs/server'],
+    },
+  });
+}
