812ca78227075154814b7301043c558816087ab6
Chris Vasey <chrismvasey@gmail.com>
2024-03-12 15:29:10 +0000

    Finished adding all content and desktop first pass

--- resources/js/Pages/LandingPage.tsx
+++ resources/js/Pages/LandingPage.tsx
@@ -21,16 +21,50 @@ export default function LandingPage() {
       content: 'Your premier technology & sales recruitment partner'
     },
     {
-      title: 'Discover',
+      title: 'Mamba Search is revolutionising talent acquisition',
+      type: 'section',
+      content: (
+        <>
+          <p className='mt-4 lg:mt-0 text-3xl mx-4 font-bold'>
+            With a unique blend of strategic expertise and compassionate <span className='text-green'>leadership</span> in the tech and sales sectors.
+            <br /><br />
+            Find top-tier talent or <span className='text-green'>advance your career</span>
+          </p>
+        </>
+      )
+    },
+    {
+      title: "At Mamba Search, we're More than just a placement agency",
       type: 'section',
       content: (
         <p className='mt-4 lg:mt-0 text-3xl mx-4 font-bold'>
-          Mamba Search is revolutionising talent <span className='text-green'>acquisition</span> with a unique blend of strategic expertise and compassionate <span className='text-green'>leadership</span> in the tech and sales sectors.
+          We're <span className='text-green'>partners in yoursuccess</span>.
+          <br />
+          With visionary leadership, we prioritize <span className='text-green'>holistic growth</span>, ensuring every candidate thrives in their role.
           <br /><br />
-          Find <span className='text-green'>top-tier talent</span> or advance your career with Mamba Search today.
+          Whether you're a tech company or a sales organization, we have the resources and expertise to <span className='text-green'>meet your needs</span>.
         </p>
       )
-    }
+    },
+    {
+      title: "Experience the difference with Mamba Search.",
+      type: 'section',
+      content: (
+        <p className='mt-4 lg:mt-0 text-3xl mx-4 font-bold'>
+          Providing a unique end-to-end <span className='text-green'>health and wellbeing</span> experiences that drive continuous <span className='text-green'>growth</span> and <span className='text-green'>optimal performance</span> for our clients and candidates.
+          <br /><br />
+          Together, we <span className='text-green'>redefine success</span>, creating a community where greatness knows no bounds. Join us at Mamba Search, where <span className='text-green'>greatness is not just a goal</span>, but a way of life.
+        </p>
+      )
+    },
+    {
+      title: "Get in touch today",
+      type: 'contact',
+      button: {
+        text: 'Get Started',
+        link: 'mailto:dillon@mambasearch.com'
+      },
+    },
   ]
 
   return (
@@ -57,10 +91,26 @@ export default function LandingPage() {
               />
             )
 
+            case 'contact':
+              return (
+                <Contact
+                  key={index}
+                  title={block.title}
+                  button={block.button}
+                  odd={index % 2 != 0}
+                />
+              )
+
           default:
             break;
         }
       })}
+
+      <div className=' bg-white/10 py-3'>
+        <p className='text-center text-white/30 text-base'>
+          &copy; {new Date().getFullYear()} <span className='text-green'>Mamba Search</span>. All Rights Reserved.
+        </p>
+      </div>
     </>
   );
 }
@@ -90,26 +140,59 @@ const Hero = ({ title, content }: {
 )
 
 const Section = ({ title, content, odd }: {
-  title: string,
+  title?: JSX.Element | string | null,
   content: JSX.Element
   odd: boolean
 }) => (
   <div className={clsx(
-    'relative min-h-screen flex flex-col lg:grid grid-cols-1 lg:grid-cols-2 gap-x-4 mx-4 items-center justify-center',
-    odd && 'bg-white/10'
+    'relative min-h-screen flex flex-col lg:grid grid-cols-1 gap-x-4 mx-4 items-center justify-center',
+    odd && 'bg-white/10',
+    title && 'lg:grid-cols-2',
   )}>
-    <div className='flex h-100 col-span-1'>
-      <div className='w-full max-w-[900px]'>
-        <div className='text-center lg:text-left mx-8'>
-          <h1 className='text-[5.5rem] leading-[5.5rem] font-bold'>
-            {title}
-          </h1>
+    {title && (
+      <div className='flex h-100 col-span-1'>
+
+        <div className='w-full max-w-[900px]'>
+
+          <div className='text-center lg:text-left mx-8'>
+            <h1 className='text-[5rem] leading-[5rem] font-bold'>
+              {title}
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
+    )}
+    <div className={clsx(!title && 'text-center')}>
       {content}
     </div>
+  </div>
+)
+
+const Contact = ({ title, button, odd }: {
+  title?: JSX.Element | string | null,
+  button: {
+    text: string
+    link: string
+  }
+  odd: boolean
+}) => (
+  <div className={clsx(
+    'relative min-h-screen mx-4 flex flex-col items-center justify-center gap-8',
+    odd && 'bg-white/10',
+  )}>
+    <img className='h-auto w-full max-w-[280px] mx-auto block' src={mambaLogo} />
+    <div className='mx-8'>
+            <h1 className='text-[5rem] leading-[5rem] font-bold'>
+              {title}
+            </h1>
+      </div>
+      <a
+        href={button.link}
+        className="rounded-md bg-green px-3.5 py-2.5 text-2xl font-semibold  shadow-sm white:bg-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green ring-2 ring-green  hover:bg-transparent hover:text-green"
+      >
+        {button.text}
+      </a>
+
   </div>
 )
 
